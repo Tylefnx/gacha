2f7e9bc1c3bc8b48edb08723a613a24567bf3a40
tylefnx <tayfunucuncu@gmail.com>
2025-06-27 02:03:04 +0300

    removed unnecessary colors

--- lib/app_colors.dart
+++ lib/app_colors.dart
@@ -5,13 +5,6 @@ class AppColors {
 
   static const Color purple = Color(0xFF6A0DAD);
   static const Color darkPurple = Color(0xFF330066);
-  static const Color secondary = Color(0xFFB39DDB);
-  static const Color background = Color(0xFFF3E5F5);
-  static const Color textPrimary = Color(0xFF212121);
-  static const Color textSecondary = Color(0xFF757575);
-  static const Color accent = Color(0xFFFFC107);
-  static const Color error = Color(0xFFD32F2F);
-  static const Color success = Color(0xFF388E3C);
   static const Color darkOrange = Color(0xFFE49344);
   static const Color yellow = Color(0xFFF9D681);
   static const Color darkYellow = Color(0xFFFBC521);
